f5d06b7540db4813f764bb6efe4691a05f1f01b5
JonathanRys <jonathan.rk.rys@gmail.com>
2022-06-20 12:58:23 -0400

    set server timezone to EST for now to test effects

--- server.ts
+++ server.ts
@@ -5,6 +5,9 @@ if (process.env.NODE_ENV !== "production") {
   require("./mocks");
 }
 
+// Set server timezone
+process.env.TZ = 'America/New_York';
+
 export const handler = createRequestHandler({
   build,
   mode: process.env.NODE_ENV,
