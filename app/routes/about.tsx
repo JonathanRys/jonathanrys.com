4808e576e05a1ceb55d2d705d9c9d32624be6559
JonathanRys <jonathan.rk.rys@gmail.com>
2024-11-10 20:27:03 -0500

    make some minor content updates

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -21,7 +21,7 @@ const About = () => {
     {
       id: 3,
       title: "About Me",
-      text: "I've lived in the Boston area my whole life. I learned to use a computer so I could play video games at a very young age.  I remember using DOS 3.0 on a 16 GHz machine.  I learned BASIC in High school and I taught myself C and wrote a text-based video game with some animated graphics.  Then one summer I helped my father to rewrite some FORTRAN code to simulate the chemical reactions in the gasoline blending process to be compatible with the version of FORTRAN used by Aspen.  This required rewriting all the loops to use a newer syntax and all I had was a dot-matrix printed FORTRAN manual to help me.  After getting the code working in the flowcharting software, we were able to run simulations and adjust the inputs to find the optimal solution.  We then took that solution and used it to program EEPROM chips for use in the refinery's control systems.  This got me excited about a career in computer science.",
+      text: "I've lived in the Boston area my whole life. I learned to use a computer so I could play video games at a very young age.  I remember using DOS 3.0 on a 16 GHz machine.  I learned BASIC in High school and I taught myself C and wrote a text-based video game with some animated graphics.  Then one summer I helped my father to rewrite some FORTRAN code and used the results to program EEPROM chips.  This got me excited about a career in computer science.",
     },
   ];
 
--- app/routes/portfolio.tsx
+++ app/routes/portfolio.tsx
@@ -11,7 +11,7 @@ const portfolioData = [
     id: 1,
     title: "HealthVision",
     company: "HDAI",
-    text: "Improved developer experience and worked on all aspects of the application available through Epic. This app allows physicians and nurses to gain valuable insights in to their patients' medical history using data science and AI tools.",
+    text: "Improved developer experience and worked on all aspects of the application available in hospitals throughout the country inside Epic. This app allows physicians and nurses to gain valuable insights into their patients' medical history using data science and AI tools.",
   },
   {
     id: 2,
@@ -29,7 +29,7 @@ const portfolioData = [
     id: 4,
     title: "Queuing system",
     company: "BetterLesson",
-    text: "Took ownership of the queuing system and managed setting up a FIFO queue as well as dead-letter queues. This was built in AWS Lambda (Python) and SQS.",
+    text: "Took ownership of the queuing system and managed setting up a FIFO queue as well as dead-letter queues. This was built in AWS Lambda(Python) and SQS.",
   },
   {
     id: 5,
@@ -41,25 +41,25 @@ const portfolioData = [
     id: 6,
     title: "Python 3 Upgrade",
     company: "BetterLesson",
-    text: "Led the upgrade of our servers in all environments from Python 2.7 to Python 3.6",
+    text: "Led the upgrade of our servers in all environments from Python 2.7 to Python 3.6 and updated the encoding and collation of some columns in MySQL to utf8 from latin1.",
   },
   {
     id: 7,
     title: "Arduino project for my parents",
     company: "Mom & Dad",
-    text: "Wrote C++ code to control the lights on the stairs at my parent's house.  This required using a priority queue so that someone triggering one set of lights doesn't affect another trigger.",
+    text: "Wrote C++ code to control the lights on the stairs at my parent's house using motion sensors.  This required using a priority queue so that someone triggering one set of lights doesn't affect another set of previously triggered lights.",
   },
   {
     id: 8,
-    title: "OnSite",
-    company: "American Tower",
-    text: "This was an app to help technicians take inventory of the equipment on cell phone towers. The app needed to be available offline so they could upload the data when they returned to their homes. This app was built with jQuery, CSS, and HTML.",
-  },
-  {
-    id: 9,
     title: "Mobile app to check product ingredients",
     company: "Personal Project",
     text: "Designed and build a mobile app for Android using React Native.  This app allows the user to set dietary preferences within the app and then scan food products at the store.  The app takes the barcode and calls the OpenFoodFacts.org API to get a list of ingredients and other dietary information.  Then it makes a call to my Python API to classify the ingredients.  Then, based on the user's preferences, it would let them know if the product met their approval.",
+  },
+  {
+    id: 9,
+    title: "OnSite",
+    company: "American Tower",
+    text: "This was an app to help technicians take inventory of the equipment on cell phone towers. The app needed to be available offline so they could upload the data when they returned to their homes. This app was built with jQuery, CSS, and HTML.  I was not initially the architect on this project, but I came to own the product.",
   },
   {
     id: 10,
@@ -71,7 +71,7 @@ const portfolioData = [
     id: 11,
     title: "Texaco Project",
     company: "Texaco",
-    text: "Rewrote legacy FORTRAN code to work with Aspen.  Used the results to program EEPROM chips for us in the refinery control systems.",
+    text: "Rewrote legacy FORTRAN code to work with the inputs and outputs of the flowcharting software Aspen.  Used the results to program EEPROM chips for use in refinery control systems.  This required rewriting all the loops to use a newer syntax and all I had was a dot-matrix printed FORTRAN manual to help me.  After getting the code working in the flowcharting software, we were able to run simulations and adjust the inputs to find the optimal solution.  We then took that solution and used it to program EEPROM chips for use in the refinery's control systems.",
   },
 ];
 
@@ -89,54 +89,6 @@ const Portfolio = () => {
             <p>{folio.text}</p>
           </div>
         ))}
-        {/* 
-        <h5 className="mb-2 mt-5 text-base font-semibold">HealthVision</h5>
-        <p>
-          Improved developer experience and worked on all aspects of the
-          application available through Epic. This app allows physicians and
-          nurses to gain valuable insights in to their patients' medical history
-          using data science and AI tools.
-        </p>
-        <h5 className="mb-2 mt-5 text-base font-semibold">PhysGPT</h5>
-        <p>
-          This project ingested millions of vectors of data from over 5000 PDFs
-          that were OCRed, split into chunks, embedded, and uploaded to
-          Pinecone. The vectors were then queried and fed to Chat GPT to answer
-          questions about physics for a group of mad scientists. This was built
-          with FastAPI and React then provisioned on AWS using Terraform.
-        </p>
-        <h5 className="mb-2 mt-5 text-base font-semibold">jonathan.rys.com</h5>
-        <p>I built this site using ReMix.</p>
-        <h5 className="mb-2 mt-5 text-base font-semibold">Queuing system</h5>
-        <p>
-          Took ownership of the queuing system and managed setting up a FIFO
-          queue as well as dead-letter queues. This was build in Python and SQS.
-        </p>
-        <h5 className="mb-2 mt-5 text-base font-semibold">
-          BetterLesson web site
-        </h5>
-        <p>
-          Worked on various features of our interactive coaching platform, from
-          video integration with Zoom to user management consoles. This was
-          Built on Flask and React.
-        </p>
-        <h5 className="mb-2 mt-5 text-base font-semibold">Python 3 Upgrade</h5>
-        <p>
-          Led the upgrade of our servers in all environments from Python 2.7 to
-          Python 3.6
-        </p>
-        <h5 className="mb-2 mt-5 text-base font-semibold">OnSite</h5>
-        <p>
-          This was an app to help technicians take inventory of the equipment on
-          cell phone towers. The app needed to be available offline so they
-          could upload the data when they returned to their homes. This app was
-          built with jQuery, CSS, and HTML.
-        </p>
-        <h5 className="mb-2 mt-5 text-base font-semibold">Digits</h5>
-        <p>
-          This was a digital learning platform that I helped work on. It was
-          built with HTML, CSS, MathML, and Javascript.
-        </p> */}
       </div>
     </div>
   );
